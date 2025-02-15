536b4bb9e420adc84873cfdea7f7029814203935
iclaudiaadeline <a.iradukun3@alustudent.com>
2025-02-15 19:47:29 +0200

    adding feature to make it extract data from external file

--- script.js
+++ script.js
@@ -1,34 +1,34 @@
-import text from "./01_data-extraction.js";
+import text from "./extractData.js";
 
-const emailRegex = /[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}/g;
-const emails = text.match(emailRegex);
-console.log({emails});
+const emailRegex = /(?<= )[^ ]+@.+?(?= )/g
+const emails = text.match(emailRegex)
+console.log({emails})
 
-const urlRegex = /https?:\/\/[^\s]+/g;
-const urls = text.match(urlRegex);
-console.log({urls});
+const urlRegex = /http.+?(?= )/g
+const urls = text.match(urlRegex)
+console.log({urls})
 
-const phoneRegex = /\(?\d{3}\)?[\s.-]?\d{3}[\s.-]?\d{4}/g;
-const phones = text.match(phoneRegex);
-console.log({"Phone numbers": phones});
+const phoneRegex = /\(?\d{3}\)?[\.\- ]\d{3}[\.\- ]\d{4}/g
+const phones = text.match(phoneRegex)
+console.log({"Phone numbers": phones})
 
-const cardRegex = /\b\d{4}([\- ])\d{4}\1\d{4}\1\d{4}\b/g;
-const cards = text.match(cardRegex);
-console.log({"Credit card numbers": cards});
+const cardRegex = /\d{4}([\- ])\d{4}\1\d{4}\1\d{4}/g
+const cards = text.match(cardRegex)
+console.log({"Credit card numbers": cards})
 
-const timeRegex = /\b(?:[01]?\d|2[0-3]):[0-5]\d(?: [APap][Mm])?\b/g;
-const times = text.match(timeRegex);
-console.log({Time: times});
+const timeRegex = /\d{1,2}\:\d{1,2}( [AP]M)?/g
+const times = text.match(timeRegex)
+console.log({Time: times})
 
-const htmlRegex = /<[^>]+>/g;
-const htmls = text.match(htmlRegex);
-console.log({"HTML tags": htmls});
+const htmlRegex = /<.+?>/g
+const htmls = text.match(htmlRegex)
+console.log({"HTML tags": htmls})
 
-const hashRegex = /#[a-zA-Z0-9_]+/g;
-const hashes = text.match(hashRegex);
-console.log({Hashtags: hashes});
+const hashRegex = /#.+?(?= )/g
+const hashes = text.match(hashRegex)
+console.log({Hashtags: hashes})
 
-const currRegex = /\$\d{1,3}(?:,\d{3})*(?:\.\d{2})?/g;
-const currs = text.match(currRegex);
-console.log({"Currency amounts": currs});
+const currRegex = /\$[1-9\.\,]+/g
+const currs = text.match(currRegex)
+console.log({"Currency amounts": currs})
 
